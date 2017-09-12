64e6ed83981848866b057c9057717e235bd2cfd2
Tammy Ernst <TammyKErnst@yahoo.com>
2017-09-12 06:39:40 +0000

    Added html header to overview page

--- web/overview.py
+++ web/overview.py
@@ -5,7 +5,18 @@ def doIt( environ, start_response ) :
     response_headers = [('Content-type','text/html')]
     start_response( '200 OK', response_headers)
   
-    page_content = "<h1>Task list list</h1>\n"
+    page_content = '<!DOCTYPE html>\n\
+\n\
+<html lang="en-US">\n\
+    <head>\n\
+        <title>Taligen script</title>\n\
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">\n\
+\n\
+        <link rel="stylesheet" href="'+config.CONTEXT+'/css/default.css">\n\
+    </head>'
+    
+    page_content += '<body>'
+    page_content += "<h1>Task list list</h1>\n"
     # f = []
     for (dirpath, dirnames, filenames) in os.walk(config.JSONDIR):
         # f.extend(filenames)
@@ -17,5 +28,6 @@ def doIt( environ, start_response ) :
 			basefname = os.path.splitext(filename)[0]
 			page_content += '<li><a href="'+config.CONTEXT+'/render'+reldir+'/'+basefname+'">'+basefname+'</a></li>'
         page_content += "</ol>"
+    page_content += '</body>'
 
     return page_content
