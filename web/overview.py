7c11ef409f15484a24d3ec14bdb26ad8e5df5deb
Tammy Ernst <TammyKErnst@yahoo.com>
2017-09-12 06:09:00 +0000

    Added task list list to overview page

--- web/overview.py
+++ web/overview.py
@@ -1,6 +1,21 @@
+import os
+import config
 
 def doIt( environ, start_response ) :
-  response_headers = [('Content-type','text/html')]
-  start_response( '200 OK', response_headers)
+    response_headers = [('Content-type','text/html')]
+    start_response( '200 OK', response_headers)
+  
+    page_content = "<h1>Task list list</h1>\n"
+    # f = []
+    for (dirpath, dirnames, filenames) in os.walk(config.JSONDIR):
+        # f.extend(filenames)
+        reldir = dirpath[len(config.JSONDIR):]
+        page_content += "<h2>"+reldir+"</h2>"
+        page_content += "<ol>"
+        # print ("dirpath: " + dirpath + ", dirnames: " + str(dirnames) + ", filenames: " + str(filenames))
+        for filename in filenames:
+			basefname = os.path.splitext(filename)[0]
+			page_content += '<li><a href="'+config.CONTEXT+'/render'+reldir+'/'+basefname+'">'+basefname+'</a></li>'
+        page_content += "</ol>"
 
-  return '<p>This is the overview page.</p>'
+    return page_content
